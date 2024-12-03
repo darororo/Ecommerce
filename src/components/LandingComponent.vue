b04400aebbbfa51d1822bf4d0b65c66c94ff07b6
Menghuor <hourousa9@gmail.com>
2024-12-03 22:44:30 +0700

    Add a login button that directs to the website with a specified route

--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -264,14 +264,16 @@ export default {
 }
 
 .showcase-item {
-    width: 1600px;
+    height: 100%;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     background-color: purple;
 }
 
 .showcase-item > img {
-    width: 100%;
+    width: auto;
+    height: 100%;
 }
+
 
 
 .bg{
