c11d4df1d788ae03218c3dc09d6d9218da0219ec
darororo <daroyem99@gmail.com>
2024-12-03 15:57:11 +0700

    Set up routers to HomePage and AuthPage

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+import UserForm from '../components/UserForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      component: HomePage,
     },
+
+    {
+      path: '/login',
+      component: UserForm,
+      props: {
+        isCreateForm: false
+      }
+    },
+
+    {
+      path: '/signup',
+      component: UserForm,
+      props: {
+        isCreateForm: true,
+      }
+    }
 
   ],
 })
