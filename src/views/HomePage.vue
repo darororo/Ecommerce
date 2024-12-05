64fec85c5255ad6e9f19b7d80ba7fe0baed76757
darororo <daroyem99@gmail.com>
2024-12-05 17:03:45 +0700

    Render PopularCard in HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,18 +2,21 @@
   <NavComponent/>
   <LandingComponent/>
   <ProductList/>
+  <PopularCard />
 </template>
 
 <script>
 import LandingComponent from '../components/LandingComponent.vue';
 import NavComponent from '../components/NavComponent.vue';
+import PopularCard from '../components/PopularCard.vue';
 import ProductList from '../components/ProductList.vue';
 
 export default {
     components: {
         LandingComponent,
         NavComponent,
-        ProductList
+        ProductList,
+        PopularCard
     }
 }
 
