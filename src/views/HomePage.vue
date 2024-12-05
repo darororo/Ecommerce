05a6750fe601d9735091deab8576ec7e84ad44a5
darororo <daroyem99@gmail.com>
2024-12-05 17:15:38 +0700

    Update NavComponent to have dynamic colors

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,9 @@
 <template>
-  <NavComponent/>
-  <LandingComponent/>
+  <Transition appear @enter="navEnter" >
+    <NavComponent class="nav" :textColor="color"/>
+  </Transition>
+  <LandingComponent class="hero"/>
+
   <ProductList/>
   <PopularCard />
   <FooterComponent/>
@@ -12,15 +15,72 @@ import LandingComponent from '../components/LandingComponent.vue';
 import NavComponent from '../components/NavComponent.vue';
 import PopularCard from '../components/PopularCard.vue';
 import ProductList from '../components/ProductList.vue';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default {
+    setup() {
+        gsap.registerPlugin(ScrollTrigger);
+    },
+    data() {
+      return {
+        color: 'white',
+      }
+    },
     components: {
-        LandingComponent,
-        NavComponent,
-        ProductList,
-        PopularCard,
-        FooterComponent
+      LandingComponent,
+      NavComponent,
+      ProductList,
+      PopularCard
+    },
+    methods: {
+      navInit,
+      navEnter,
+      navLeave,
+    },
+
+}
+
+
+function navInit(el, done) {
+  this.color = "white"
+  done()
+}
+
+function navEnter(el, done){
+  console.log("enter")
+  gsap.to(el, {
+    scrollTrigger: {
+      trigger: ".hero",
+      start: "bottom top",
+      toggleActions: "play none reverse none",
+      markers: true,
+
+    },
+    duration: 0,
+    onComplete: () => {
+      this.color = "black";
+      done();
+    },
+    onReverseComplete: () => {
+      this.color = "white"
+      done();
     }
+  })
+}
+
+function navLeave(el, done){
+  // gsap.to(el, {
+  //   duration: 0,
+  //   onComplete: () => {
+  //     this.color = "black"
+  //   },
+  //   onReverseComplete: () => {
+  //     this.color = "black"
+  //   }
+  // })
+
+  this.color = white;
 }
 
 </script>
