f3d6ec4e33c95689aa6bb8983d5caa1f2736e5ce
Menghuor <hourousa9@gmail.com>
2024-12-26 21:35:47 +0700

    Add chevron Back Icon in CarHeader

---
+++ src/components/icons/productPage/chevronBack.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg width="46" height="29" viewBox="0 0 40 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M27.1508 5.76797L24.7431 4.3125L12.8389 11.5L24.7431 18.6875L27.1508 17.232L17.6617 11.5L27.1508 5.76797Z" fill="black"/>
+    </svg>
+</template>
