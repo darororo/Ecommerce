f30dcf7c82e062e534688d8ef4d70566c09ebacd
YORNsoksunnara <163433515+YORNsoksunnara@users.noreply.github.com>
2024-12-30 17:10:18 +0700

    feat: icons for EngineDetails

---
+++ src/components/icons/nameIcon/engine.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_277_121)">
+<path d="M14.4 6.71988V4.79988H16.8V2.87988H10.08V4.79988H12.48V6.71988" stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round"/>
+<path d="M4.31998 12.9601H2.39998V10.0801H0.47998V17.7601H2.39998V14.8801H4.31998" stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round"/>
+<path d="M21.6 9.6002V12.0002H20.16V8.08484C20.16 7.59668 19.7707 7.2002 19.2917 7.2002H9.59521C9.36529 7.2002 9.14353 7.2938 8.98081 7.46036L6.72001 10.0802H5.28001C4.80001 10.0802 4.32001 10.5602 4.32001 11.0402V16.8002C4.32001 17.2888 4.80097 17.7602 5.28001 17.7602H6.72001L8.98177 20.3815C9.14449 20.5471 9.36529 20.6402 9.59521 20.6402H15.7656C15.9955 20.6402 16.2163 20.5471 16.379 20.3815L19.9051 16.7901C20.0688 16.624 20.1595 16.3994 20.1595 16.1647V14.8802H21.5995V17.2802H23.5195V9.6002H21.6Z" stroke="black" stroke-width="2.0077" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M15.36 13.4396L11.52 18.7196L12.96 14.3996H10.56L12.96 9.59961H15.36L12.96 13.4396H15.36Z" fill="black"/>
+</g>
+<defs>
+<clipPath id="clip0_277_121">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+
+</template>
