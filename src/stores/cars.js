48de9400926298a9a413c97da6cf43f3b73552ca
darororo <daroyem99@gmail.com>
2025-01-13 22:15:59 +0700

    fix: filter based on price correctly
    *fix previous commit bug
    
    *filter based on discounted price

--- src/stores/cars.js
+++ src/stores/cars.js
@@ -6,41 +6,41 @@ export const useCarStore = defineStore("cars", {
     serverConnected: false,
     brands: ["Ferrari", "Mercedes", "Lamborghini", "Porsche", "BMW"],
     cars: [
-      // {
-      //   id: "1",
-      //   price: 6670088,
-      //   discount: 10,
-      //   brand: "Ferrari",
-      //   model: "2017 Ferrari LaFerrari",
-      //   description:
-      //     "The LaFerrari features a 6262cc V12 engine built for performance. The LaFerrari is the first production car to ever be equipped with an F1-derived hybrid solution",
-      //   innerDescription: [
-      //     "The LaFerrari features a 6262cc V12 engine built for performance. The LaFerrari is the first production car to ever be equipped with an F1-derived hybrid solution and combines a powerful electric motor with the classic Ferrari V12 engine. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quaerat voluptates animi ad iure eos magnam doloremque. Soluta delectus voluptatibus velit odit, vero quae pariatur, voluptatem sit veniam dicta vitae!",
-      //     "The LaFerrari Aperta is a hypercar with a hybrid powertrain that combines a V12 engine with an electric motor. It was introduced in 2017 to mark Ferrari's 70th anniversary. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sed esse quaerat, necessitatibus provident nesciunt repellendus quae fuga omnis explicabo sit sint amet tempora totam quidem et, sequi nisi? Ullam.",
-      //   ],
-      //   images: [
-      //     "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car1.png",
-      //     "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car2.png",
-      //     "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car3.png",
-      //     "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car4.png",
-      //     "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car5.png",
-      //   ],
-      //   engine: {
-      //     year: 2017,
-      //     mileage: 0,
-      //     power: 949,
-      //     ownerNumbers: 1,
-      //     cylinder: 12,
-      //     color: "Red",
-      //     location: "Dubai",
-      //     fuelType: "Petrol",
-      //   },
-      //   location: "Ferrari Centre Phnom Penh City, Cambodia",
-      //   status: "Ferrari Approved Pre-Owned",
-      //   financeDetails: {
-      //     interestRate: "2.99 p.a.flat",
-      //   },
-      // },
+      {
+        id: "1",
+        price: 6670088,
+        discount: 10,
+        brand: "Ferrari",
+        model: "2017 Ferrari LaFerrari",
+        description:
+          "The LaFerrari features a 6262cc V12 engine built for performance. The LaFerrari is the first production car to ever be equipped with an F1-derived hybrid solution",
+        innerDescription: [
+          "The LaFerrari features a 6262cc V12 engine built for performance. The LaFerrari is the first production car to ever be equipped with an F1-derived hybrid solution and combines a powerful electric motor with the classic Ferrari V12 engine. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quaerat voluptates animi ad iure eos magnam doloremque. Soluta delectus voluptatibus velit odit, vero quae pariatur, voluptatem sit veniam dicta vitae!",
+          "The LaFerrari Aperta is a hypercar with a hybrid powertrain that combines a V12 engine with an electric motor. It was introduced in 2017 to mark Ferrari's 70th anniversary. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sed esse quaerat, necessitatibus provident nesciunt repellendus quae fuga omnis explicabo sit sint amet tempora totam quidem et, sequi nisi? Ullam.",
+        ],
+        images: [
+          "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car1.png",
+          "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car2.png",
+          "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car3.png",
+          "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car4.png",
+          "/Ecommerce/src/assets/images/products/Ferrari-Laferrari/car5.png",
+        ],
+        engine: {
+          year: 2017,
+          mileage: 0,
+          power: 949,
+          ownerNumbers: 1,
+          cylinder: 12,
+          color: "Red",
+          location: "Dubai",
+          fuelType: "Petrol",
+        },
+        location: "Ferrari Centre Phnom Penh City, Cambodia",
+        status: "Ferrari Approved Pre-Owned",
+        financeDetails: {
+          interestRate: "2.99 p.a.flat",
+        },
+      },
 
       {
         id: "2",
@@ -200,9 +200,13 @@ export const useCarStore = defineStore("cars", {
 
       if (price) {
         if (price > 1000000) {
-          filteredCars = filteredCars.filter((c) => c.price >= 1000000);
+          filteredCars = filteredCars.filter(
+            (c) => this.getDiscountedPrice(c.price, c.discount || 0) > price
+          );
         } else if (price <= 1000000) {
-          filteredCars = filteredCars.filter((c) => c.price < 1000000);
+          filteredCars = filteredCars.filter(
+            (c) => this.getDiscountedPrice(c.price, c.discount || 0) <= price
+          );
         }
       }
 
@@ -219,33 +223,6 @@ export const useCarStore = defineStore("cars", {
       console.log(price, brand, isDiscount);
 
       return filteredCars;
-
-      // const filteredCars = this.cars.filter((car) => {
-      //   let priceMatched;
-      //   if (price <= 1000000) {
-      //     priceMatched = car.price <= price;
-      //   } else if (price > 1000000) {
-      //     priceMatched = car.price > price;
-      //   } else {
-      //     priceMatched = true; // if price is undefined
-      //   }
-
-      //   console.log("brancd check: ", car.brand.toLowerCase() === brand);
-
-      //   const brandMatched = brand
-      //     ? car.brand.toLowerCase() === brand.toLowerCase()
-      //     : true;
-      //   const isDiscountMatched = isDiscount ? car.discount > 0 : true;
-
-      //   console.log(priceMatched, brandMatched, isDiscountMatched);
-      //   if (priceMatched && brandMatched && isDiscountMatched)
-      //     console.log(
-      //       "returning",
-      //       car.id,
-      //       priceMatched && brandMatched && isDiscountMatched
-      //     );
-      //   return priceMatched && brandMatched && isDiscountMatched;
-      // });
     },
   },
 });
